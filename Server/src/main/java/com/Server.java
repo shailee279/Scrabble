ef80ca3214c61acfea64c52e1c99584977924bd8
cn-wx <121894598@qq.com>
2018-09-18 15:16:12 +1000

    update gitignore

---
+++ Server/src/main/java/com/Server.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @author xuwang < xuwang2@student.unimelb.edu.au >
+ * @id 979895
+ * @date 2018/9/18 15:15
+ */
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
